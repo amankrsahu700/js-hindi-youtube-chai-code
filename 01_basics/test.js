44db6d0dc56a172591ec6803d300dabf211c505b
Aman Kumar Sahu <153420903+amankrsahu700@users.noreply.github.com>
2026-05-29 11:25:37 +0000

    variables study

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -2,3 +2,7 @@ console.log("aman")
 let a = 'aman';
 let b = 24;
 console.log(a+b)
+let c = 15;
+let d = 20;
+let aman = (c + d);
+console.log(aman)
---
+++ 01_basics/var.js
@@ -0,0 +1,9 @@
+const accountId = 23032440004;
+let accountEmail = "amankr0008138@gmail.com"
+var accountPassward = 113114
+accountCity = "Dhanbad"
+
+
+console.log([accountEmail,accountId,accountPassward]);
+console.table([accountId,accountPassward,accountEmail,accountCity]);
+ 

3bbbc93ac845b9844ec876019a08c9233744890b
Aman Kumar Sahu <153420903+amankrsahu700@users.noreply.github.com>
2026-05-31 08:42:20 +0000

    dtypes

---
+++ 01_basics/dtypes.js
@@ -0,0 +1,20 @@
+//alert(3+3); we are using node js not browser
+
+console.log(3+3);
+console.log("aman");
+
+let name = 'aman'       //string
+let age = 23            // number 
+let isLoggedIn = false //boolean
+                        //bigInt
+                        //null
+                        // undefined
+                        //symbol => unique
+                        // object
+console.log(typeof"aman")//type of
+
+
+
+
+
+

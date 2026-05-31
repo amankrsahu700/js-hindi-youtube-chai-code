cafa653b8180a0c17bdaf34cb656c01fc5b591ae
Aman Kumar Sahu <153420903+amankrsahu700@users.noreply.github.com>
2026-05-31 12:32:20 +0000

    conversion2.0 operations

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -17,3 +17,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
 
 
+// ******************* - operation - *****************//
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/3)
+console.log(2%3)
+console.log(2**3)
+
+let str1 = "Hello"
+let str2 = "aman"
+
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2"+2) 
+
+console.log(1 + 2 + "2")
+
+console.log(+true);
+
+console.log(+"")
+
+let num1, num2, num3 = 2
+console.log(num1+num2+num3);  
+
+let gCounter = 100;
+for (let index = 0; index <= 7; index++) {
+    console.log(gCounter++)
+    
+}
+
+// gCounter++;
+// console.log(gCounter);
+
